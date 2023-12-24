2730ba3fc37211c5b9c4d93892add945a1dc321a
anuragrudra81 <137820809+anuragrudra81@users.noreply.github.com>
2023-12-24 23:18:51 +0600

    Ready To Publish

--- wp-admin/profile.php
+++ wp-admin/profile.php
@@ -1,18 +1,12 @@
-<?php
-/**
- * User Profile Administration Screen.
- *
- * @package WordPress
- * @subpackage Administration
- */
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://anuragrudra81.github.io/Demo-Newspaper/login/?redirect_to=https%3A%2F%2Fanuragrudra81.github.io%2FDemo-Newspaper%2Fwp-admin%2Fprofile.php%3Fsimply_static_page%3D830&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">window.location = "https://anuragrudra81.github.io/Demo-Newspaper/login/?redirect_to=https%3A%2F%2Fanuragrudra81.github.io%2FDemo-Newspaper%2Fwp-admin%2Fprofile.php%3Fsimply_static_page%3D830&reauth=1";</script>
 
-/**
- * This is a profile page.
- *
- * @since 2.5.0
- * @var bool
- */
-define( 'IS_PROFILE_PAGE', true );
-
-/** Load User Editing Page */
-require_once __DIR__ . '/user-edit.php';
+		<p>You are being redirected to <a href="https://anuragrudra81.github.io/Demo-Newspaper/login/?redirect_to=https%3A%2F%2Fanuragrudra81.github.io%2FDemo-Newspaper%2Fwp-admin%2Fprofile.php%3Fsimply_static_page%3D830&reauth=1">https://anuragrudra81.github.io/Demo-Newspaper/login/?redirect_to=https%3A%2F%2Fanuragrudra81.github.io%2FDemo-Newspaper%2Fwp-admin%2Fprofile.php%3Fsimply_static_page%3D830&reauth=1</a></p>
+	</body>
+</html>
